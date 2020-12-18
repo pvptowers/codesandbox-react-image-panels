0aa2d19ae4fba08b3e69cea6fb7d37b9488621ba
Paul Towers <40795207+pvptowers@users.noreply.github.com>
2020-12-18 15:06:14 +1100

    update to panel headings

--- src/App.js
+++ src/App.js
@@ -2,36 +2,29 @@ import React, { useEffect, useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  const panelHeadings = [
-    "Heading One",
-    "Heading Two",
-    "Heading Three",
-    "Heading Four"
-  ];
-
   const panelHeadings2 = [
     {
-      title: "Heading One",
+      title: "By the coast",
       url:
         "https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
     },
     {
-      title: "Heading One",
+      title: "In the woods",
       url:
         "https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
     },
     {
-      title: "Heading One",
+      title: "Next to the ocean",
       url:
         "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80"
     },
     {
-      title: "Heading One",
+      title: "Looking at the harbour",
       url:
         "https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"
     },
     {
-      title: "Mountains",
+      title: "By the Mountains",
       url:
         "https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
     }
@@ -40,19 +33,11 @@ export default function App() {
   return (
     <div className="App">
       <div class="container">
-        {/* {panelHeadings.map((t) => (
-          <PanelDiv>{t}hi</PanelDiv>
-        ))} */}
-        {panelHeadings2.map(
-          (t) => (
-            console.log(t.url),
-            (
-              <PanelDiv myStyle={{ backgroundImage: `url(${t.url})` }}>
-                {t.title}
-              </PanelDiv>
-            )
-          )
-        )}
+        {panelHeadings2.map((el) => (
+          <PanelDiv myStyle={{ backgroundImage: `url(${el.url})` }}>
+            <h3>{el.title}</h3>
+          </PanelDiv>
+        ))}
       </div>
     </div>
   );
